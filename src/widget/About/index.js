74ef06c841f89c366f9598d3c60b01c73206a38c
Ambrish-git <mycodinglibrary@gmail.com>
2019-06-22 07:13:59 +0530

    test

--- src/widget/About/index.js
+++ src/widget/About/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 
 class Welcome extends React.Component {
   render() {
-    return <h1>Hello World from About??</h1>;
+    return <h1>Hello World from About</h1>;
   }
 }
 
