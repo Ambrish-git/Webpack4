2ac62df3ffb4e56a6c56cdca6f251476bb891a72
Ambrish-git <mycodinglibrary@gmail.com>
2019-06-22 07:35:14 +0530

    plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,9 @@
+const  {CleanWebpackPlugin}  = require('clean-webpack-plugin');
+const WriteFilePlugin =require('write-file-webpack-plugin') ;
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const webpack = require("webpack");
 
 module.exports = {
     entry: {
@@ -6,23 +12,70 @@ module.exports = {
     },
     output: {
       path: __dirname + '/dist',
-      filename: '[name].bundle.js'
+      filename: '[name].js'
     },
     devServer: {
         open:true,
     },
+    // resolve: {
+    //   extensions: ['', '.js', '.jsx']
+    // },
     module: {
       rules: [
         {
           test: /\.(js|jsx)$/,
           exclude: /node_modules/,
           use: ['babel-loader']
+        },
+        {
+          test: /\.css$/,
+          use: [MiniCssExtractPlugin.loader, 'css-loader'],
         }
       ]
     },
+    optimization: {
+       minimizer: [new TerserPlugin({
+        parallel: true,sourceMap: true,
+      })],
+      splitChunks: {
+          cacheGroups: {
+              default: false,
+              vendors: false,
+              // vendor chunk
+              vendor: {
+                name: 'vendor',
+                  // sync + async chunks
+                  chunks: 'all',
+                  // import file path containing node_modules
+                  test: /node_modules/,
+                  priority: 20,
+              },
+              // common chunk
+              common: {
+                name: 'common',
+                minChunks: 2,
+                chunks: 'all',
+                priority: 10,
+                reuseExistingChunk: true,
+                enforce: true
+              }
+
+          }
+      }},
     plugins: [
-      
-            
+      new CleanWebpackPlugin(),
+      new WriteFilePlugin(),
+      new BundleAnalyzerPlugin(),
+
+      new webpack.DefinePlugin({
+        PRODUCTION: JSON.stringify(true),
+        ENVIRONMENT_VAR: JSON.stringify(' ENV 1.0'), 
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+       
+      }),
+      new MiniCssExtractPlugin({
+        filename: '[name].css',
+      }),
           ],
     devtool: 'inline-source-map',
   };
